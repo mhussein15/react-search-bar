100df8951dc148526719a8b1c0fe07c812a0bd8c
Mohammad Hussein <mhalfakih15@gmail.com>
2021-01-24 15:09:49 +0300

    task done

---
+++ src/App.js
@@ -0,0 +1,43 @@
+// Styling
+import { GlobalStyle, ThemeButton } from "./styles";
+
+import Home from "./components/Home";
+// Components
+import ProductList from "./components/ProductList";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+
+const theme = {
+  light: {
+    mainColor: "#242424", // main font color
+    backgroundColor: "#fefafb", // main background color
+    pink: "#ff85a2",
+    red: "#ff3232",
+  },
+  dark: {
+    mainColor: "#fefafb", // main font color
+    backgroundColor: "#242424", // main background color
+    pink: "#ff85a2",
+    red: "#ff3232",
+  },
+};
+
+function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const toggleTheme = () =>
+    setCurrentTheme(currentTheme === "light" ? "dark" : "light");
+
+  return (
+    <ThemeProvider theme={theme[currentTheme]}>
+      <GlobalStyle />
+      <ThemeButton onClick={toggleTheme}>
+        {currentTheme === "light" ? "Dark" : "Light"} Mode
+      </ThemeButton>
+      <Home />
+      <ProductList />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,62 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const Description = styled.h4`
+  text-align: center;
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${(props) => props.theme.mainColor};
+    background-color: ${(props) => props.theme.backgroundColor}
+  }
+`;
+
+export const ListWrapper = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+`;
+
+export const ProductWrapper = styled.div`
+  margin: 20px;
+
+  img {
+    width: 200px;
+    height: 200px;
+  }
+
+  p {
+    text-align: center;
+
+    &.product-price {
+      color: ${(props) => props.theme.pink};
+    }
+  }
+`;
+
+export const SearchBarStyled = styled.input`
+  padding: 0.5rem;
+  margin: 1rem auto;
+  display: block;
+  width: 40%;
+`;
+
+export const ShopImage = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+`;
+
+export const ThemeButton = styled.button`
+  font-size: 1em;
+  margin: 1.25em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme.mainColor};
+  color: ${(props) => props.theme.backgroundColor};
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+`;
